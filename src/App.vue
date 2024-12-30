bfea2d12ebd3f1c7ae3534f3114454ec18990c80
Saurabh Saini <saurabhsaini.it@gmail.com>
2024-12-30 23:15:22 +0530

    Router

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,12 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <router-link to="/">Home</router-link>
+     |
+    <router-link to="/about">About</router-link>
+
+    <router-view></router-view>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,25 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
 import {createRouter, createWebHistory} from "vue-router";
+import App from './App.vue';
+import Home from '@/views/Home.vue';
+import About from "@/views/About.vue";
+
 
 const router = createRouter({
     history: createWebHistory(),
-    routes: []
+    routes: [
+        {
+            path: '/',
+            name: 'Home',
+            component: Home,
+        },
+        {
+            path: '/about',
+            name: 'About',
+            component: About,
+        }
+    ]
 });
 createApp(App)
     .use(router)
